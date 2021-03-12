517e5081d77dbcc0b8d85e02901c0d812058bd51
Karina Kantuni <avagyan_mika@mail.ru>
2021-03-12 20:04:39 +0400

    Working on todo app on react native

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
 // import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Navbar } from './src/Navbar';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Navbar } from "./src/Navbar";
+import { AddTodo } from "./src/AddTodo";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title,
+    // };
+
+    //setTodos(todos.concat([newTodo]))
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Navbar title="ToDo APP"/>
+    <View>
+      <Navbar title="ToDo APP" />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <View>
+          {todos.map((todo) => {
+            return <Todo todo={todo} key={todo.id} />;
+          })}
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
 });
---
+++ src/AddTodo.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, StyleSheet, TextInput, Button } from "react-native";
+
+export const AddTodo = ({ onSubmit }) => {
+  const pressHandler = () => {
+    onSubmit("Test Todo");
+  };
+
+  return (
+    <View style={styles.block}>
+      <TextInput style={styles.input} />
+      <Button title="Add" onPress={pressHandler} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  input: {
+    width: "80%",
+    padding: 10,
+    borderStyle: "solid",
+    borderBottomWidth: 2,
+    borderBottomColor: "blue",
+  },
+});
